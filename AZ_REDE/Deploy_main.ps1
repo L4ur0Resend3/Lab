4236539af170b632307450db4d7ce153e8624552
Lauro Resende <lauro.resende@outlook.com>
2021-09-22 22:04:07 -0300

    All works
    :)

--- AZ_REDE/Deploy_main.ps1
+++ AZ_REDE/Deploy_main.ps1
@@ -1,6 +1,19 @@
-$templateFile = "C:\scripts\LabL\Lab\AZ_REDE\azuredeploy.json"
+clear-host
+write-host "Atualizando Vnet-localiza..."
+$templateFile = "C:\scripts\LabL\Lab\AZ_REDE\template.json"
 $today=Get-Date -Format "MM-dd-yyyy"
-$deploymentName="MainDeploy-"+"$today"
+$deploymentName="MainDeployVnet"+"$today"
 New-AzResourceGroupDeployment `
   -Name $deploymentName `
   -TemplateFile $templateFile
+
+
+
+  write-host "Atualizando VNG..."
+$templateFile = "C:\scripts\LabL\Lab\AZ_REDE\vng.json"
+$today=Get-Date -Format "MM-dd-yyyy"
+$deploymentName="MainDeploygateway"+"$today"
+New-AzResourceGroupDeployment `
+  -Name $deploymentName `
+  -TemplateFile $templateFile
+#>
